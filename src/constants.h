a31a4c4e8f633b10434444aa055f7813792cd762
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-03-21 14:35:35 +0200

    Added constants to module load

---
+++ src/constants.h
@@ -0,0 +1,7 @@
+#include <node.h>
+#include <node_buffer.h>
+
+class CubebConstants {
+public:
+	static void Initialize (v8::Handle<v8::Object> target);
+};
--- src/module.cpp
+++ src/module.cpp
@@ -1,11 +1,13 @@
 #include <node.h>
 
+#include "constants.h"
 #include "context.h"
 #include "stream.h"
 
 extern "C" void init (v8::Handle<v8::Object> target) {
 	v8::HandleScope scope;
 
+	CubebConstants::Initialize(target);
 	CubebContext::Initialize(target);
 	CubebStream::Initialize(target);
 }
