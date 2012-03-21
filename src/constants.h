0ba4e5e027a873c4e373c73954e7eaae9c22e9fc
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-03-21 15:23:01 +0200

    Proper library definition

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,6 @@
+#ifndef NODE_CUBEB_CONSTANTS_H
+#define NODE_CUBEB_CONSTANTS_H
+
 #include <node.h>
 #include <node_buffer.h>
 
@@ -5,3 +8,5 @@ class CubebConstants {
 public:
 	static void Initialize (v8::Handle<v8::Object> target);
 };
+
+#endif
